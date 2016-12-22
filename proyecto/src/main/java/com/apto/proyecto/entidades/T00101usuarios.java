d5b27796251b5f02717ab44ca1cd7d25f6d08eba
crrb <crrb@localhost>
2016-12-22 16:58:27 -0500

    iniciando

---
+++ proyecto/src/main/java/com/apto/proyecto/entidades/T00101usuarios.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.apto.proyecto.entidades;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author crrb
+ */
+@Entity
+@Table(name = "T001_01USUARIOS")
+@NamedQueries({
+    @NamedQuery(name = "T00101usuarios.findAll", query = "SELECT t FROM T00101usuarios t")})
+public class T00101usuarios implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID_USUARIO")
+    private BigDecimal idUsuario;
+    @Size(max = 200)
+    @Column(name = "NOMBRE")
+    private String nombre;
+    @Size(max = 200)
+    @Column(name = "APELLIDO")
+    private String apellido;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Size(max = 300)
+    @Column(name = "EMAIL")
+    private String email;
+    @Size(max = 20)
+    @Column(name = "CELULAR")
+    private String celular;
+    @Size(max = 150)
+    @Column(name = "DIRECCION")
+    private String direccion;
+
+    public T00101usuarios() {
+    }
+
+    public T00101usuarios(BigDecimal idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public BigDecimal getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(BigDecimal idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idUsuario != null ? idUsuario.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof T00101usuarios)) {
+            return false;
+        }
+        T00101usuarios other = (T00101usuarios) object;
+        if ((this.idUsuario == null && other.idUsuario != null) || (this.idUsuario != null && !this.idUsuario.equals(other.idUsuario))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.apto.proyecto.entidades.T00101usuarios[ idUsuario=" + idUsuario + " ]";
+    }
+    
+}
